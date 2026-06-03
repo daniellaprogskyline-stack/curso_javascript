16f4be6aae5f76817cb1aa0133fa1d0ba44a8fc8
daniellaprogskyline-stack <daniellaprog.skyline@gmail.com>
2026-06-03 14:56:11 -0300

    -- feat: exercicio 18

---
+++ atv18/script3.js
@@ -0,0 +1,29 @@
+function Contar(){
+    let ini = getDocumentById ('txti')
+    let fim = getDocumentById ('txtf')
+    let passo = getDocumentById ('txtp')
+}
+
+if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar!'
+   //window.alert('[ERRO] Faltam dados')
+} else {
+    res.innerHTML = "Contando: <br>"
+    let i = Number(ini.value)
+    let f = Number (fim.value)
+    let p = Number(passo.value)
+    if (p <= 0) {
+        window.alert('Passo inválido! Considerando PASSO 1.')
+    }
+    if (i < f){
+         for(let c = i; c <= f; c+=p) {
+        res.innerHTML += `${c} \u{1F603}`
+    }
+    res.innerHTML  += `${c} \u{1F603}`
+    } else {
+        for (let c =  i; c>= f; c-=p) {
+            res.innerHTML+= `${c}`
+        }
+    }
+   
+}
